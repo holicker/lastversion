05f921da2e9b999c76a04ed60d2f890ff1fe9722
gnehs91@nate.com <rh2112us^^>
2021-11-18 21:33:30 +0900

    proxy url modified

--- front/src/lib/api/client.js
+++ front/src/lib/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-axios.defaults.baseURL = "http://localhost/api";
+axios.defaults.baseURL = "http://ip-172-31-36-36.ap-northeast-2.compute.internal/api";
 const client = axios.create();
 /*
   글로벌 설정 예시:
